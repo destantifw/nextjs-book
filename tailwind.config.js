914fd5035f32cd30ad81ccf0c252061706588c4a
destanti <destantifw82@gmail.com>
2023-09-14 07:33:55 +0700

    fix tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,8 @@
-module.exports = {
-  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {
-      colors: {
-        primary: '#00AFF4',
-        secondary: '#FF3287',
-        tertiary: '#C87296'
-      }
-    }
-  },
-  plugins: [require('@tailwindcss/typography')]
-};
+import('tailwindcss').Config
+
+export const content = []
+export const theme = {
+  extend: {},
+}
+export const plugins = []
+
